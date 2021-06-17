916f62bc315ea78cf35074952a2b76cbaa02b4d2
Michael Mulich <michael.mulich@gmail.com>
2021-06-17 09:52:46 -0700

    abstract the use of underlying libraries
    This just makes it so that the gumby library is referenced
    rather than the underlying library.

--- gumby/__init__.py
+++ gumby/__init__.py
@@ -1,6 +1,9 @@
 # Expose the major parts of the elasticsearch library
 from elasticsearch import Elasticsearch as Client
 
+# Make the DSL interface importable as 'dsl'
+import elasticsearch_dsl as dsl
+
 # Expose gumby's internals
 from .factories import *
 from .models import *
