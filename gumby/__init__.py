4957bd15e6d937a2b3952d2e3eb1ef0ce9508273
Michael Mulich <michael.mulich@gmail.com>
2021-07-26 17:23:51 -0700

    Allow discovery of the elasticsearch hosts by ELASTICSEARCH_HOSTS env-var

--- gumby/__init__.py
+++ gumby/__init__.py
@@ -1,7 +1,16 @@
+from os import getenv
+
 # Expose the major parts of the elasticsearch library
-from elasticsearch import Elasticsearch as Client
+from elasticsearch import Elasticsearch as BaseClient
 
 # Expose gumby's internals
 from . import dsl
 from .factories import *
 from .models import *
+
+
+class Client(BaseClient):
+    def __init__(self, hosts=None, **kwargs):
+        if not hosts:
+            hosts = getenv('ELASTICSEARCH_HOSTS', '').split(',')
+        super().__init__(hosts=hosts, **kwargs)
